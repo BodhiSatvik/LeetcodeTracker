41aca8f0ac7fb9a5c2a3efb6b8608c0c4068d10f
Satvik Bhasin <47910500+hack-m@users.noreply.github.com>
2022-06-29 01:14:41 -0400

    Time: 44 ms (65.63%), Space: 13.9 MB (65.54%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,7 +1,15 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         ans = 0
-        for value in collections.Counter(s).values():
+        hesh = {}
+        for i in s:
+            if i in hesh:
+                hesh[i] += 1
+            else:
+                hesh[i] = 1
+        
+    
+        for value in hesh.values():
             ans += value //2*2
             if (ans % 2 == 0) and (value%2==1):
                 ans += 1
