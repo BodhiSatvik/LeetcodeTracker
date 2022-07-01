c090854c4391aece636fc6893e90bdca5b9fa6ac
Satvik Bhasin <47910500+hack-m@users.noreply.github.com>
2022-06-30 20:47:26 -0400

    Time: 62 ms (71.75%), Space: 14.5 MB (34.68%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if collections.Counter(s) != collections.Counter(t):
-            return False
-        return True
+        # if collections.Counter(s) != collections.Counter(t):
+        #     return False
+        hesh1 = {}
+        hesh2 = {}
+        for i in s:
+            if i in hesh1:
+                hesh1[i] += 1
+            else:
+                hesh1[i] = 1
+        
+        for i in t:
+            if i in hesh2:
+                hesh2[i] += 1
+            else:
+                hesh2[i] = 1
+        
+        return hesh1 == hesh2
