47f39c7eb3f8d3a5fa91285f867de0c4dd51fe7d
Satvik Bhasin <47910500+hack-m@users.noreply.github.com>
2022-11-01 19:15:02 +0800

    Time: 239 ms (61.43%), Space: 15 MB (64.72%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,16 +4,50 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
-        if not t: return True
-        if not s: return False
-        if self.same(s, t):
-            return True
-        return (self.isSubtree(s.left, t) or self.isSubtree(s.right, t))
+# class Solution:
+#     def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+#         if not t: return True
+#         if not s: return False
+#         if self.same(s, t):
+#             return True
+#         return (self.isSubtree(s.left, t) or self.isSubtree(s.right, t))
     
-    def same(self, s, t):
-        if not s and not t: return True
-        if s and t and s.val == t.val:
-            return (self.same(s.left, t.left) and self.same(s.right, t.right))
-        return False
+#     def same(self, s, t):
+#         if not s and not t: return True
+#         if s and t and s.val == t.val:
+#             return (self.same(s.left, t.left) and self.same(s.right, t.right))
+#         return False
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        # Check for all subtree rooted at all nodes of tree "root"
+        def dfs(node):
+
+            # If this node is Empty, then no tree is rooted at this Node
+            # Thus "tree rooted at node" cannot be same as "rooted at subRoot"
+            # "tree rooted at subRoot" will always be non-empty (constraints)
+            if node is None:
+                return False
+
+            # If "tree rooted at node" is identical to "tree at subRoot"
+            elif is_identical(node, subRoot):
+                return True
+
+            # If "tree rooted at node" was not identical.
+            # Check for tree rooted at children
+            return dfs(node.left) or dfs(node.right)
+
+        def is_identical(node1, node2):
+
+            # If any one Node is Empty, both should be Empty
+            if node1 is None or node2 is None:
+                return node1 is None and node2 is None
+
+            # Both Nodes Value should be Equal
+            # And their respective left and right subtree should be identical
+            return (node1.val == node2.val and
+                    is_identical(node1.left, node2.left) and
+                    is_identical(node1.right, node2.right))
+
+        # Check for node rooted at "root"
+        return dfs(root)
