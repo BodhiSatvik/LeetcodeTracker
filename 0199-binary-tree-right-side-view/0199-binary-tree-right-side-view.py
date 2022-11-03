9deb14cd699e7095c2ba2e937b752520d929044c
Satvik Bhasin <47910500+hack-m@users.noreply.github.com>
2022-11-03 10:57:35 +0800

    Time: 67 ms (29.68%), Space: 13.9 MB (71.44%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,29 +4,47 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        q = collections.deque()
-        low = 0
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+#         q = collections.deque()
+#         low = 0
         
-        if root:
-            q.append([root,1])
+#         if root:
+#             q.append([root,1])
             
-        while q:
-            for i in range(len(q)):
-                node, level = q.pop()
+#         while q:
+#             for i in range(len(q)):
+#                 node, level = q.pop()
                 
-                if level > low:
-                    res.append(node.val)
-                    low = level
+#                 if level > low:
+#                     res.append(node.val)
+#                     low = level
 
-                if node.left:
-                    q.append([node.left, level+1])
+#                 if node.left:
+#                     q.append([node.left, level+1])
                 
-                if node.right:
-                    q.append([node.right, level+1])
+#                 if node.right:
+#                     q.append([node.right, level+1])
         
+#         return res
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
         return res
                     
                 
