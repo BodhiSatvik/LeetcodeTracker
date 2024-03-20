0b900b83ea245b9158abaad122f3bff2f53c8ec0
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-03-20 15:53:17 -0400

    Time: 1130 ms (59.32%) | Memory: 0B (100.00%) - LeetSync

--- 1181-game-play-analysis-iii/game-play-analysis-iii.sql
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-select a1.player_id, a1.event_date, sum(a2.games_played) games_played_so_far
-from Activity a1
-join Activity a2
-on a1.player_id = a2.player_id and a1.event_date>= a2.event_date
-group by a1.player_id, a1.event_date
-order by a1.player_id, a1.event_date
+select player_id, event_date, sum(games_played) over (partition by player_id order by event_date) games_played_so_far
+    from Activity
+
