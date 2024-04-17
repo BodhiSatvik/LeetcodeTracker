2632199f20f54cc69e56c58901f1a6a8ee4c444f
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-04-17 10:11:11 -0400

    Time: 269 ms (31.13%) | Memory: 17.7 MB (61.78%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,27 +1,20 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(s)< len(p):
+        ns, np = len(s), len(p)
+        if ns < np:
             return []
-        letters = collections.defaultdict(int)
-        target = {}
-        res = []
-        for i in range(len(p)):
-            target[p[i]] = 1 + target.get(p[i], 0)
-        n = len(p)
-        l, r = 0, n-1
-        for i in range(n):
-            letters[s[i]] = 1 + letters.get(s[i], 0)
-        if letters == target:
-            res.append(l)
-        while r < len(s)-1:
-            letters[s[l]] -= 1
-            if letters[s[l]] == 0:
-                del letters[s[l]]
-            l += 1
-            r += 1
-            if r < len(s):
-                letters[s[r]] = 1 + letters.get(s[r], 0)
-            if letters == target:
-                res.append(l)
-        return res
 
+        countS, countP = collections.Counter(), collections.Counter(p)
+        res = []
+
+        for i in range(ns):
+            countS[s[i]] += 1
+
+            if i >= np:
+                if countS[s[i-np]] == 1:
+                    del countS[s[i-np]]
+                else:
+                    countS[s[i-np]] -= 1
+            if countP == countS:
+                res.append(i-np+1)
+        return res
