0b547531dabea201b250f51a30eaea99deeac906
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-05-08 09:30:01 -0400

    Time: 645 ms (74.37%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,17 +1,18 @@
 # Write your MySQL query statement below
-with all_amt as (
-    select visited_on, sum(amount) total
+with all_days as (
+    select visited_on, sum(amount) as total
     from customer
     group by visited_on
-    ),
+), 
 rolling as (
-    select  visited_on,
-            round(sum(total) over (order by visited_on rows between 6 preceding and current row), 2) as amount,
-            round(avg(total) over (order by visited_on rows between 6 preceding and current row), 2) as average_amount
-    from all_amt
+    select visited_on, 
+           round(sum(total) over (order by visited_on rows between 6 preceding and current row), 2) as amount,
+           round(avg(total) over (order by visited_on rows between 6 preceding and current row), 2) as average_amount
+    from all_days
+    group by visited_on
+    order by visited_on
 )
 
-select * 
+select *
 from rolling
 where visited_on >= (select visited_on from customer order by visited_on limit 1) + 6
-order by visited_on
