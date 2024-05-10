bcb37b029b207ea64e9242a8447edcc4ac838446
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-05-10 11:22:39 -0400

    Time: 76 ms (83.85%) | Memory: 17 MB (30.22%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                removeL, removeR = s[l+1:r+1], s[l:r]
+                return removeL == removeL[::-1] or removeR == removeR[::-1]
+            l += 1
+            r -= 1
+        return True
+        
