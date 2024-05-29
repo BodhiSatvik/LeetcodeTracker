a52c4321d772130a8d1d39e2170b19ef844adb5a
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-05-29 14:19:54 -0400

    Time: 165 ms (80.69%) | Memory: 17.9 MB (76.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -11,8 +11,6 @@ class Solution:
                 count += 1
             else:
                 count -= 1
-                if count == 0:
-                    candidate = num
-                    count += 1
+                
         
         return candidate
