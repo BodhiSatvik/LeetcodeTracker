90a3121a0ac5958b3bc50897ffd9fbc6f9c13bea
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-05-29 14:15:38 -0400

    Time: 151 ms (99.41%) | Memory: 17.9 MB (96.64%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority = collections.Counter(nums)
-        return max(majority, key=majority.get)
+        count = 0
+        candidate = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
