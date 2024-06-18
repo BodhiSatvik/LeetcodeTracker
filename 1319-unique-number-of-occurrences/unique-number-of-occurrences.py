bea883e31ccb2e6996fb79e17108bb7b3181a192
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-06-18 15:10:27 -0400

    Time: 47 ms (19.91%) | Memory: 16.8 MB (20.76%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,10 +1,15 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        a = collections.Counter(arr)
-        ase = set()
-        for v in a.values():
-            if v in ase:
-                return False
-            ase.add(v)
-        return True
+        # a = collections.Counter(arr)
+        # ase = set()
+        # for v in a.values():
+        #     if v in ase:
+        #         return False
+        #     ase.add(v)
+        # return True
+        freq = {}
+        for x in arr:
+            freq[x] = freq.get(x, 0) + 1
+
+        return len(freq) == len(set(freq.values()))
         
