c29a94e8ccdf39398ac197b502fdd94af8dcd91e
Satvik Bhasin <47910500+BodhiSatvik@users.noreply.github.com>
2024-06-20 15:05:46 -0400

    Time: 38 ms (67.76%) | Memory: 16.6 MB (27.85%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,9 +4,6 @@ class Solution:
         r = x
         while l <= r:
             m = (l+r)//2
-            print("m", m)
-            print("l", l)
-            print("r", r)
             if m*m == x:
                 return m
             elif m*m > x:
